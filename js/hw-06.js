0bf850da123d674a16f137df934e4ceefa09b12b
Joseph Gawler <joey@Josephs-MacBook-Air.local>
2023-03-01 00:21:52 -0500

    functional_bar_highlight

--- js/hw-06.js
+++ js/hw-06.js
@@ -48,34 +48,16 @@ const Y_SCALE2 = d3.scaleLinear()
 .domain([0, 3]) // add some padding  
 .range([VIS_HEIGHT, 0]);
 
-// Define scale functions that maps our data values 
-// (domain) to pixel values (range)
-const X_SCALE3 = d3.scaleLinear() 
-.domain([0, 4]) // add some padding  
-.range([0, VIS_WIDTH]); 
-
-const Y_SCALE3 = d3.scaleLinear() 
-.domain([0, 60]) // add some padding  
-.range([VIS_HEIGHT, 0]);
 
 function color(type) {
     if (type == "setosa") {
-        return "green"
-    }
-    if (type == 1) {
-      return "blue"
+        return "blue"
     }
     if (type == "versicolor") {
         return "orange"
     }
-    if (type == 2) {
-      return "orange"
-    }
     if (type == "virginica") {
-        return "blue"
-    }
-    if (type == 3) {
-      return "green"
+        return "green"
     }
 }
 
@@ -90,6 +72,14 @@ d3.csv("/data/iris.csv").then((data) => {
       .attr("cy", (d) => { return (Y_SCALE1(d.Petal_Length) + MARGINS.bottom); }) 
       .attr("r", 3)
       .attr('fill', d => color(d.Species));
+
+    FRAME1.append("text")
+      .attr("x", (VIS_WIDTH - 150))             
+      .attr("y", (MARGINS.top / 2))
+      .attr("text-anchor", "middle")  
+      .style("font-size", "16px") 
+      .style("text-decoration", "underline")  
+      .text("Petal_Length vs. Sepal_Length");
 
   // Add an x axis to the vis  
   FRAME1.append("g") 
@@ -114,36 +104,61 @@ d3.csv("/data/iris.csv").then((data) => {
       .attr("r", 3)
       .attr('fill', d => color(d.Species));
 
-  // Add an x axis to the vis  
-  FRAME2.append("g") 
+    FRAME2.append("text")
+      .attr("x", (VIS_WIDTH - 150))             
+      .attr("y", (MARGINS.top / 2))
+      .attr("text-anchor", "middle")  
+      .style("font-size", "16px") 
+      .style("text-decoration", "underline")  
+      .text("Petal_Length vs. Sepal_Length");
+
+    // Add an x axis to the vis  
+    FRAME2.append("g") 
         .attr("transform", "translate(" + MARGINS.left + 
               "," + (VIS_HEIGHT + MARGINS.top) + ")") 
         .call(d3.axisBottom(X_SCALE2).ticks(10)) 
           .attr("font-size", '10px');
   
-  // add y axis to the vis
-  FRAME2.append("g") 
+    // add y axis to the vis
+    FRAME2.append("g") 
         .attr("transform", "translate(" + MARGINS.left + 
               "," + (VIS_HEIGHT - 250) + ")") 
         .call(d3.axisLeft(Y_SCALE2).ticks(10)) 
           .attr("font-size", '10px');
 
-  const flowers = [1, 2, 3];
+  
+  const X_SCALE3 = d3.scaleBand() 
+  .domain(data.map(d => d.Species))
+  .range([0, VIS_WIDTH]); 
+
+  const Y_SCALE3 = d3.scaleLinear() 
+  .domain([0, 60]) // add some padding  
+  .range([VIS_HEIGHT, 0]);
+
+
   var Plot3 = FRAME3.selectAll(".bar") // ADD PLOT3
-        .data(flowers)
+        .data(data)
         .enter()
         .append("rect")                   
-            .attr("x", (d) => { return (X_SCALE3(d) + MARGINS.left - 25); })
+            .attr("x", (d) => { return (X_SCALE3(d.Species) + 90); })
             .attr("y", (d) => { return (MARGINS.bottom + 50); })
             .attr("width", 50)
-            .attr("height", (d) => { return (Y_SCALE3(1) - 45); })
-            .attr('fill', d => color(d))
-        
+            .attr("height", (d) => { return (250); })
+            .attr('fill', d => color(d.Species))
+            
+    FRAME3.append("text")
+        .attr("x", (VIS_WIDTH - 150))             
+        .attr("y", (MARGINS.top / 2))
+        .attr("text-anchor", "middle")  
+        .style("font-size", "16px") 
+        .style("text-decoration", "underline")  
+        .text("Count of Species");
+
     // Add an x axis to the vis
     FRAME3.append("g") 
         .attr("transform", "translate(" + MARGINS.left + 
                 "," + (VIS_HEIGHT + MARGINS.top) + ")") 
-        .call(d3.axisBottom(X_SCALE3).ticks(3)) 
+        .call(d3.axisBottom(X_SCALE3)) 
         .attr("font-size", '10px');
           
     // add y axis to the vis
@@ -155,7 +170,6 @@ d3.csv("/data/iris.csv").then((data) => {
 
 
 
-    // brushed scatterplot resource: https://observablehq.com/@d3/brushable-scatterplot
     FRAME2
       .call( d3.brush()                 // Add the brush feature using the d3.brush function
         .extent( [ [50,50], [FRAME_WIDTH, FRAME_HEIGHT - 50] ] ) // initialise the brush area: start at 0,0 and finishes at width,height: it means I select the whole graph area
@@ -171,7 +185,7 @@ d3.csv("/data/iris.csv").then((data) => {
           .filter(d => x0 <= (X_SCALE2(d.Sepal_Width) + MARGINS.left) && 
           (X_SCALE2(d.Sepal_Width) + MARGINS.left) < x1 && y0 <= (Y_SCALE2(d.Petal_Width) + MARGINS.bottom) && 
           (Y_SCALE2(d.Petal_Width) + MARGINS.bottom) < y1) // filter sets the size of the selected box and that selected points align with user selection.
-          .style("stroke", "red")
+          .style("stroke", "darkorange")
           .style("opacity", 1)
           .data();
 
@@ -181,16 +195,19 @@ d3.csv("/data/iris.csv").then((data) => {
           .filter(d => x0 <= (X_SCALE2(d.Sepal_Width) + MARGINS.left) && 
           (X_SCALE2(d.Sepal_Width) + MARGINS.left) < x1 && y0 <= (Y_SCALE2(d.Petal_Width) + MARGINS.bottom) && 
           (Y_SCALE2(d.Petal_Width) + MARGINS.bottom) < y1) // filter sets the size of the selected box and that selected points align with user selection.
-          .style("stroke", "orange")
+          .style("stroke", "darkorange")
           .style("opacity", 1)
           .data();
 
         value = Plot3
-          .style("stroke", "none") // Initial stroke of non-selected bars
+          .style("opacity", 0.5) // Initial stroke of non-selected bars
+          .style("stroke", "none")
           .filter(d => x0 <= (X_SCALE2(d.Sepal_Width) + MARGINS.left) && 
           (X_SCALE2(d.Sepal_Width) + MARGINS.left) < x1 && y0 <= (Y_SCALE2(d.Petal_Width) + MARGINS.bottom) && 
           (Y_SCALE2(d.Petal_Width) + MARGINS.bottom) < y1) // filter sets the size of the selected box and that selected points align with user selection.
-          .style("stroke", "orange")
+          .style("opacity", 1)
+          .style("stroke", "darkorange")
+          .style("stroke-width", "6px")
           .data();
 
       }
